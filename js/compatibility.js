feb988a821638d72d0f151c484507ec40e62b162
Evan Goode <mail@evangoo.de>
2015-04-05 15:24:02 -0400

    made the world a better place.

--- js/compatibility.js
+++ js/compatibility.js
@@ -1,3 +1,3 @@
-if (!(window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/'))) {
+if (window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1) {
   window.location.replace('http://bigassmessage.com/75e88');
 }
