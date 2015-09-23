d3258ff78912e86533299eb2f1a698fc33cec893
Evan Goode <mail@evangoo.de>
2015-09-22 22:37:43 -0400

    Lay's Wavy

--- mailsetup.sh
+++
@@ -1,6 +0,0 @@
-sudo postconf -e 'smtpd_sasl_local_domain ='
-sudo postconf -e 'smtpd_sasl_auth_enable = yes'
-sudo postconf -e 'smtpd_sasl_security_options = noanonymous'
-sudo postconf -e 'broken_sasl_auth_clients = yes'
-sudo postconf -e 'smtpd_recipient_restrictions = permit_sasl_authenticated,permit_mynetworks,reject_unauth_destination'
-sudo postconf -e 'inet_interfaces = all'
